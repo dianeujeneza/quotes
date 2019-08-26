7b554cc3a2c3750410ddc6e71293999c4a940d3d
Diane Ujeneza <dianesis17@gmail.com>
2019-08-26 09:09:39 +0200

    edited the form

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,7 @@
 export class Quote {
+ 
     showName: boolean;
-  constructor(public id: number,public name: string,public description: string, public completeDate: Date){
+  constructor(public id: number,public name: string,public description: string,public sname:string, public completeDate: Date){
     this.showName=false;
   }
 }
